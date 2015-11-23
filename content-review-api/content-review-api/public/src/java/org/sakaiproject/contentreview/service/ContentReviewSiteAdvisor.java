2e2d795d6b0b42b320b627e84fe6bd0d93d85dff
bgarciaentornos <bgarcia@entornosdeformacion.com>
2015-11-23 09:08:13 +0100

    First upload
    Limited functionalities

---
+++ content-review-api/content-review-api/public/src/java/org/sakaiproject/contentreview/service/ContentReviewSiteAdvisor.java
@@ -0,0 +1,11 @@
+package org.sakaiproject.contentreview.service;
+
+import org.sakaiproject.site.api.Site;
+
+public interface ContentReviewSiteAdvisor {
+
+	
+	public boolean siteCanUseReviewService(Site site);
+	
+	public boolean siteCanUseLTIReviewService(Site site);
+}
